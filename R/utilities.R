5b0e396f028139f9a4141032a7ba44da4bbb771a
Colin Millar <colin@ices.dk>
2019-06-12 17:57:10 +0200

    move startup utils to a common script

---
+++ R/utilities.R
@@ -0,0 +1,30 @@
+library(data.tree)
+library(icesTAF)
+
+repoName <- "cod.27.47d20"
+
+if (dir.exists(repoName)) {
+  unlink(repoName, recursive = TRUE)
+}
+
+formatRcode <- function(x) {
+  c("\`\`\`r",
+    x,
+    "\`\`\`")
+}
+
+printDir <- function(paths = NULL) {
+  if (is.null(paths)) {
+    # get skeleton path structure
+    paths <- dir(repoName, full.names = TRUE, recursive = TRUE)
+  }
+  
+  # make a data.tree and print it
+  tree <- as.Node(data.frame(pathString = paths))
+  res <- capture.output(print(as.data.frame(tree), row.names = FALSE))[-1]
+  cat(formatRcode(res), sep = "\n")
+}
+
+taflink <- function(x) {
+  sprintf("[`%1$s`](https://rdrr.io/cran/icesTAF/man/%1$s.html)", x)
+}
